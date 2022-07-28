b2d10be9fd1b1b21e259f2c7dcf7d509ac0a8bb2
frenchkebab <tubowangin@gmail.com>
2022-07-28 14:32:47 +0900

    [3. 숫자야구 #4] 숫자야구 구현

--- 3.숫자야구/NumberBaseball.jsx
+++ 3.숫자야구/NumberBaseball.jsx
@@ -3,12 +3,20 @@ const { Component } = require('react');
 import Try from './Try';
 
 // get 4 numbers randomly
-function getNumbers() {}
+function getNumbers() {
+  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i++) {
+    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
+}
 class NubmerBaseball extends Component {
   state = {
     result: '',
     value: '',
-    answer: getNumbers(),
+    answer: getNumbers(), // ex) [1, 3, 5, 7]
     tries: [],
   };
 
@@ -22,12 +30,54 @@ class NubmerBaseball extends Component {
     { fruit: '사과', taste: '맛없다' },
   ];
 
-  onSubmitForm = () => {
+  onSubmitForm = (e) => {
     e.preventDefault();
+    if (this.state.value === this.state.answer.join('')) {
+      this.setState({
+        result: '홈런!',
+        tries: [...this.state.tries, { try: this.state.value, result: '홈런!' }],
+      });
+      alert('게임을 다시 시작합니다.');
+      this.setState({
+        value: '',
+        answer: getNumbers(),
+        tries: [],
+      });
+    } else {
+      const answerArray = this.state.value.split('').map((v) => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+      // 10번 이상 틀린 경우
+      if (this.state.tries.length >= 9) {
+        this.setState({
+          result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`,
+        });
+        alert('게임을 다시 시작합니다.');
+        this.setState({
+          value: '',
+          answer: getNumbers(),
+          tries: [],
+        });
+      } else {
+        for (let i = 0; i < 4; i++) {
+          if (answerArray[i] === this.state.answer[i]) {
+            strike++;
+          } else if (this.state.answer.includes(answerArray[i])) {
+            ball++;
+          }
+          this.setState({
+            tries: [...this.state.tries, { try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다` }],
+          });
+        }
+      }
+    }
   };
 
   onChangeInput = (e) => {
-    setValue(e.target.value);
+    console.log(this.state.answer);
+    this.setState({
+      value: e.target.value,
+    });
   };
 
   render() {
@@ -39,8 +89,8 @@ class NubmerBaseball extends Component {
         </form>
         <div>시도: {this.state.tries.length}</div>
         <ul>
-          {this.fruits.map((el, i) => {
-            return <Try key={el.fruit + el.taste} value={el} index={i} />;
+          {this.state.tries.map((el, i) => {
+            return <Try key={`${i + 1}차 시도`} tryInfo={el} />;
           })}
         </ul>
       </>
