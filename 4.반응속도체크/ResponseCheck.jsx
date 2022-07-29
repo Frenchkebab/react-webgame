c39cbb9017692b43b76b275f547f05260f3ba25d
frenchkebab <tubowangin@gmail.com>
2022-07-29 13:53:38 +0900

    [4. 반응속도체크 #5] Hooks로 변환하기 위한 class 코드

---
+++ 4.반응속도체크/ResponseCheck.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+
+class ResponseCheck extends Component {
+  state = {
+    state: 'waiting',
+    message: '클릭해서 시작하세요.',
+    result: [],
+  };
+
+  timeout;
+  startTime;
+  endTime;
+
+  onClickScreen = () => {
+    const { state, message, result } = this.state;
+    if (state === 'waiting') {
+      this.setState({
+        state: 'ready',
+        message: '초록색이 되면 클릭하세요.',
+      });
+      this.timeout = setTimeout(() => {
+        this.setState({
+          state: 'now',
+          message: '지금 클릭',
+        });
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
+    } else if (state == 'ready') {
+      clearTimeout(this.timeout);
+      this.setState({
+        state: 'waiting',
+        message: '너무 성급하시군요! 초록색이 된 후에 클릭하세요.',
+      });
+    } else if (state === 'now') {
+      this.endTime = new Date();
+      // 반응 속도 체크
+      this.setState((prevState) => {
+        return {
+          state: 'waiting',
+          message: '클릭해서 시작하세요.',
+          result: [...prevState.result, this.endTime - this.startTime],
+        };
+      });
+    }
+  };
+
+  onReset = () => {
+    this.setState({
+      result: [],
+    });
+  };
+
+  renderAverage = () => {
+    const { result } = this.state;
+    return this.state.result.length === 0 ? null : (
+      <>
+        <div>평균 시간: {this.state.result.reduce((a, c) => a + c) / this.state.result.length}ms</div>
+        <button onClick={this.onReset}>Reset</button>
+      </>
+    );
+  };
+
+  render() {
+    const { state, message } = this.state;
+    return (
+      <>
+        <div id="screen" className={state} onClick={this.onClickScreen}>
+          {message}
+        </div>
+        {/* 따로 함수 component로 분리하는 것이 좋음! */}
+        {this.renderAverage()}
+      </>
+    );
+  }
+}
+
+export default ResponseCheck;
