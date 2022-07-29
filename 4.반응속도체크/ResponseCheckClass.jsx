d9a5d8efb7d8be84b8f81e867436520601626de1
frenchkebab <tubowangin@gmail.com>
2022-07-29 13:53:10 +0900

    [4. 반응속도체크 #5] reset 기능 추가

--- 4.반응속도체크/ResponseCheckClass.jsx
+++ 4.반응속도체크/ResponseCheckClass.jsx
@@ -44,10 +44,19 @@ class ResponseCheck extends Component {
     }
   };
 
+  onReset = () => {
+    this.setState({
+      result: [],
+    });
+  };
+
   renderAverage = () => {
     const { result } = this.state;
     return this.state.result.length === 0 ? null : (
-      <div>평균 시간: {this.state.result.reduce((a, c) => a + c) / this.state.result.length}ms</div>
+      <>
+        <div>평균 시간: {this.state.result.reduce((a, c) => a + c) / this.state.result.length}ms</div>
+        <button onClick={this.onReset}>Reset</button>
+      </>
     );
   };
 
@@ -58,6 +67,7 @@ class ResponseCheck extends Component {
         <div id="screen" className={state} onClick={this.onClickScreen}>
           {message}
         </div>
+        {/* 따로 함수 component로 분리하는 것이 좋음! */}
         {this.renderAverage()}
       </>
     );
