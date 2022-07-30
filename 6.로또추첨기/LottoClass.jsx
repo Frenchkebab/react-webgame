c9b82d9d4317da50bf21113af31cd52e09a4eec5
frenchkebab <tubowangin@gmail.com>
2022-07-30 18:18:46 +0900

    [6. 로또추첨기 #8] componentDidMount, componentDidUpdate 학습

--- 6.로또추첨기/LottoClass.jsx
+++ 6.로또추첨기/LottoClass.jsx
@@ -22,35 +22,57 @@ class Lotto extends Component {
     redo: false,
   };
 
-  timeoutes = [];
+  timeouts = [];
 
-  // 컴포넌트가 시작하자 마자 setTimeout이 시작되어야 하므로
-  componentDidMount() {
+  runTimeouts = () => {
     const { winNumbers } = this.state;
     for (let i = 0; i < this.state.winNumbers.length - 1; i++) {
-      this.timeoutes[i] = setTimeout(() => {
+      this.timeouts[i] = setTimeout(() => {
         this.setState((prevState) => {
           return { winBalls: [...prevState.winBalls, winNumbers[i]] };
         });
       }, (i + 1) * 1000);
     }
-    this.timeoutes[6] = setTimeout(() => {
+    this.timeouts[6] = setTimeout(() => {
       this.setState({
         bonus: winNumbers[6],
         redo: true,
       });
     }, 7000);
+  };
+
+  // 컴포넌트가 시작하자 마자 setTimeout이 시작되어야 하므로
+  componentDidMount() {
+    console.log('didMount');
+    this.runTimeouts();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('didUpdate');
+    // 업데이트를 하고 싶은 상황을 잘 정리해주어야 함!! (안그러면 state가 바뀔 때마다 변경됨)
+    if (this.state.winBalls.length === 0) {
+      this.runTimeouts();
+    }
   }
 
   // 컴포넌트가 제거되었는데도 setTimeout, setInterval 같은 것들이 돌아가게 되면
   // 메모리 문제가 생길 수 있으므로 컴포넌트가 제거될 시 반드시 제거를 해 주어야 함!
   componentWillUnmount() {
-    this.timeoutes.forEach((el) => {
+    this.timeouts.forEach((el) => {
       clearTimeout(el);
     });
   }
 
-  onClickRedo = () => {};
+  onClickRedo = () => {
+    // 초기화
+    this.setState({
+      winNumbers: getWinNumbers(), // 당첨 숫자들
+      winBalls: [],
+      bonus: null, // 보너스 공
+      redo: false,
+    });
+    this.timeouts = [];
+  };
 
   render() {
     const { winBalls, bonus, redo } = this.state;
