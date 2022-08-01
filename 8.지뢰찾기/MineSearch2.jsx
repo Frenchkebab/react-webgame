0ae6eb102086ba5a0fd6a7611301d29b9daac24b
frenchkebab <tubowangin@gmail.com>
2022-08-01 22:59:20 +0900

    [8. 지뢰찾기 #10] 빈칸들 한 번에 열기 (오류 있음)

--- 8.지뢰찾기/MineSearch2.jsx
+++ 8.지뢰찾기/MineSearch2.jsx
@@ -1,8 +1,7 @@
-import React, { useReducer, createContext, useMemo } from 'react';
+import React, { useEffect, useReducer, createContext, useMemo } from 'react';
 import Table from './Table';
 import Form from './Form';
 
-// 헷갈리지 않기 위해 적어둠
 export const CODE = {
   MINE: -7,
   NORMAL: -1,
@@ -22,12 +21,17 @@ export const TableContext = createContext({
 
 const initialState = {
   tableData: [],
+  data: {
+    row: 0,
+    cell: 0,
+    mine: 0,
+  },
   timer: 0,
   result: '',
   halted: true,
+  openedCount: 0,
 };
 
-// 지뢰를 심어줌
 const plantMine = (row, cell, mine) => {
   console.log(row, cell, mine);
   const candidate = Array(row * cell)
@@ -36,13 +40,11 @@ const plantMine = (row, cell, mine) => {
       return i;
     });
   const shuffle = [];
-  // 0 ~ 99 지뢰 개수만큼을 뽑아놓음
   while (candidate.length > row * cell - mine) {
     const chosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0];
     shuffle.push(chosen);
   }
   const data = [];
-  // row * cell 배열에 넣어줌
   for (let i = 0; i < row; i++) {
     const rowData = [];
     data.push(rowData);
@@ -61,56 +63,52 @@ const plantMine = (row, cell, mine) => {
   return data;
 };
 
-// actions
 export const START_GAME = 'START_GAME';
 export const OPEN_CELL = 'OPEN_CELL';
 export const CLICK_MINE = 'CLICK_MINE';
 export const FLAG_CELL = 'FLAG_CELL';
 export const QUESTION_CELL = 'QUESTION_CELL';
 export const NORMALIZE_CELL = 'NORMALIZE_CELL';
+export const INCREMENT_TIMER = 'INCREMENT_TIMER';
 
 const reducer = (state, action) => {
   switch (action.type) {
     case START_GAME:
       return {
         ...state,
+        data: {
+          row: action.row,
+          cell: action.cell,
+          mine: action.mine,
+        },
+        openedCount: 0,
         tableData: plantMine(action.row, action.cell, action.mine),
         halted: false,
+        timer: 0,
       };
-
     case OPEN_CELL: {
       const tableData = [...state.tableData];
-      tableData[action.row] = [...state.tableData[action.row]];
-      // 원래는 클릭한 칸만 불변성을 유지했지만, 어떤 칸이 열릴지 모르므로 모든 칸을 새로운 객체로 만들어줌
       tableData.forEach((row, i) => {
-        tableData[i] = [...state.tableData[i]];
+        tableData[i] = [...row];
       });
-
-      // 한 번 검사한 칸은 다시 재귀에서 검사하지 않도록
       const checked = [];
-
-      // 주변 칸들을 검사하는 함수
+      let openedCount = 0;
+      console.log(tableData.length, tableData[0].length);
       const checkAround = (row, cell) => {
-        if (
-          [CODE.OPENED, CODE.FLAG_MINE, CODE.FLAG, CODE.QUESTION_MINE, CODE.QUESTION].includes(tableData[row][cell])
-        ) {
-          // 이미 열려있거나 지뢰가 있는 칸들은 막아줌
-          return;
-        }
-
+        console.log(row, cell);
         if (row < 0 || row >= tableData.length || cell < 0 || cell >= tableData[0].length) {
-          // 상하좌우 칸이 범위를 벗어나는 경우
           return;
-        }
-
-        if (checked.includes(row + ',' + cell)) {
-          // 이미 검사한 칸이면
+        } // 상하좌우 없는칸은 안 열기
+        if (
+          [CODE.OPENED, CODE.FLAG, CODE.FLAG_MINE, CODE.QUESTION_MINE, CODE.QUESTION].includes(tableData[row][cell])
+        ) {
+          return;
+        } // 닫힌 칸만 열기
+        if (checked.includes(row + '/' + cell)) {
           return;
         } else {
-          // 검사하지 않은 칸이면
-          checked.push(row + ',' + cell);
-        }
-
+          checked.push(row + '/' + cell);
+        } // 한 번 연칸은 무시하기
         let around = [tableData[row][cell - 1], tableData[row][cell + 1]];
         if (tableData[row - 1]) {
           around = around.concat([
@@ -126,54 +124,55 @@ const reducer = (state, action) => {
             tableData[row + 1][cell + 1],
           ]);
         }
-
-        const count = around.filter((v) => [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v)).length;
-        console.log(around, count);
-        tableData[row][cell] = count;
+        const count = around.filter(function (v) {
+          return [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v);
+        }).length;
         if (count === 0) {
-          // 내가 빈칸일 경우 주변 애들을 검사
-          const near = [];
-
-          // 맨 윗칸을 클릭했을 경우 그 윗칸은 없음
-          if (row - 1 > -1) {
-            near.push([row - 1, cell - 1]);
-            near.push([row - 1, cell]);
-            near.push([row - 1, cell + 1]);
-          }
-          near.push([row, cell - 1]);
-          near.push([row, cell + 1]);
-
-          // 맨 아랫칸을 클릭했을 경우 그 아래칸은 없음
-          if (row + 1 > tableData.length) {
-            near.push([row + 1, cell - 1]);
-            near.push([row + 1, cell]);
-            near.push([row + 1, cell + 1]);
-          }
-
-          // 주변 칸들을 클릭
-          // 좌 or 우 칸이 없을 경우 undefined가 되어 사라져버림
-          /* 
-            arr[0][-1] 의 경우 그냥 undefined가 반환되지만,
-            arr[-1][0] 의 경우 undefined의 속성에 접근하므로 에러가 나오게 됨
-          */
-          near.forEach((n) => {
-            if (tableData[n[0]][n[1]] !== CODE.OPENED) {
-              // 이미 열은 칸이 아니면
-              checkAround(n[0], n[1]);
+          // 주변칸 오픈
+          if (row > -1) {
+            const near = [];
+            if (row - 1 > -1) {
+              near.push([row - 1, cell - 1]);
+              near.push([row - 1, cell]);
+              near.push([row - 1, cell + 1]);
             }
-          });
-        } else {
+            near.push([row, cell - 1]);
+            near.push([row, cell + 1]);
+            if (row + 1 < tableData.length) {
+              near.push([row + 1, cell - 1]);
+              near.push([row + 1, cell]);
+              near.push([row + 1, cell + 1]);
+            }
+            near.forEach((n) => {
+              if (tableData[n[0]][n[1]] !== CODE.OPENED) {
+                checkAround(n[0], n[1]);
+              }
+            });
+          }
         }
+        if (tableData[row][cell] === CODE.NORMAL) {
+          // 내 칸이 닫힌 칸이면 카운트 증가
+          openedCount += 1;
+        }
+        tableData[row][cell] = count;
       };
-
-      // 해당 칸 기준으로 검사
       checkAround(action.row, action.cell);
+      let halted = false;
+      let result = '';
+      console.log(state.data.row * state.data.cell - state.data.mine, state.openedCount, openedCount);
+      if (state.data.row * state.data.cell - state.data.mine === state.openedCount + openedCount) {
+        // 승리
+        halted = true;
+        result = `${state.timer}초만에 승리하셨습니다`;
+      }
       return {
         ...state,
         tableData,
+        openedCount: state.openedCount + openedCount,
+        halted,
+        result,
       };
     }
-
     case CLICK_MINE: {
       const tableData = [...state.tableData];
       tableData[action.row] = [...state.tableData[action.row]];
@@ -184,16 +183,12 @@ const reducer = (state, action) => {
         halted: true,
       };
     }
-
-    // 겉보기에는 똑같음
     case FLAG_CELL: {
       const tableData = [...state.tableData];
       tableData[action.row] = [...state.tableData[action.row]];
       if (tableData[action.row][action.cell] === CODE.MINE) {
-        // 지뢰가 있는 칸인 경우
         tableData[action.row][action.cell] = CODE.FLAG_MINE;
       } else {
-        // 지뢰가 없는 칸인 경우
         tableData[action.row][action.cell] = CODE.FLAG;
       }
       return {
@@ -201,8 +196,6 @@ const reducer = (state, action) => {
         tableData,
       };
     }
-
-    // 깃발이 있는 상태에서 물음표로 만들기 - 겉보기에는 똑같음
     case QUESTION_CELL: {
       const tableData = [...state.tableData];
       tableData[action.row] = [...state.tableData[action.row]];
@@ -216,7 +209,6 @@ const reducer = (state, action) => {
         tableData,
       };
     }
-
     case NORMALIZE_CELL: {
       const tableData = [...state.tableData];
       tableData[action.row] = [...state.tableData[action.row]];
@@ -230,7 +222,12 @@ const reducer = (state, action) => {
         tableData,
       };
     }
-
+    case INCREMENT_TIMER: {
+      return {
+        ...state,
+        timer: state.timer + 1,
+      };
+    }
     default:
       return state;
   }
@@ -240,14 +237,19 @@ const MineSearch = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { tableData, halted, timer, result } = state;
 
-  const value = useMemo(
-    () => ({
-      tableData: tableData,
-      halted: halted,
-      dispatch,
-    }),
-    [tableData, halted]
-  ); // dispatch는 항상 같게 유지되기 때문에 넣지 않아도 된다
+  const value = useMemo(() => ({ tableData, halted, dispatch }), [tableData, halted]);
+
+  useEffect(() => {
+    let timer;
+    if (halted === false) {
+      timer = setInterval(() => {
+        dispatch({ type: INCREMENT_TIMER });
+      }, 1000);
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  }, [halted]);
 
   return (
     <TableContext.Provider value={value}>
