38db251994752a54e6314a9692a7401a06d80ce2
frenchkebab <tubowangin@gmail.com>
2022-08-02 19:35:30 +0900

    [#9] TicTacTo Setup 안된것들 다시

---
+++ 7.틱택토/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDOM = require('react-dom/client');
+import TicTacTo from './TicTacToClass';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<TicTacTo />);
