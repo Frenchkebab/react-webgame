31affe9bb26c59fefe7211670e69ad302588ec52
frenchkebab <tubowangin@gmail.com>
2022-08-02 19:41:08 +0900

    [#9] Table, Tr, Td Comoponent 쪼개서 만들기

--- 7.틱택토/TicTacToClass.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TicTacTo = () => {
-  return;
-};
-
-export default TicTacTo;
--- 7.틱택토/client.jsx
+++ 7.틱택토/client.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom/client');
-import TicTacTo from './TicTacToClass';
+import TicTacTo from './TicTacToe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<TicTacTo />);
