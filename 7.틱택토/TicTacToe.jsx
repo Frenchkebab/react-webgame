4e6208fc6078a797fcb3f140d83729de6a9915e6
frenchkebab <tubowangin@gmail.com>
2022-08-02 19:45:34 +0900

    [#9] State가 많아지는 문제 발생

--- 7.틱택토/TicTacToe.jsx
+++ 7.틱택토/TicTacToe.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from './Table';
 
 const TicTacToe = () => {
+  const [winner, setWinner] = useState('initialState');
+  const [turn, setTurn] = useState('O');
+  const [tableData, setTableData] = useState(['', '', ''], ['', '', ''], ['', '', '']);
   return (
     <>
       <Table />
