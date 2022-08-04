fa50473b3daaf285e63f2084af7909feab638ead
frenchkebab <tubowangin@gmail.com>
2022-08-04 18:20:41 +0200

    [8. 지뢰찾기 #10] 왼쪽 오른쪽 클릭 로직 작성하기

--- 8.지뢰찾기/MineSearch.jsx
+++ 8.지뢰찾기/MineSearch.jsx
@@ -17,6 +17,7 @@ export const CODE = {
 // Context
 export const TableContext = createContext({
   tableData: [[], [], [], [], []],
+  halted: true,
   dispatch: () => {},
 });
 
@@ -25,19 +26,110 @@ const initialState = {
   tableData: [],
   timer: 0,
   result: '',
+  halted: false,
 };
 
 // action 이름 정의
 export const START_GAME = 'START_GAME';
+export const OPEN_CELL = 'OPEN_CELL';
+export const CLICK_MINE = 'CLICK_MINE';
+export const FLAG_CELL = 'FLAG_CELL';
+export const QUESTION_CELL = 'QUESTION_CELL';
+export const NORMALIZE_CELL = 'NORMALIZE_CELL';
 
 // reducer 함수 (action에 따라 할 동작 정의)
 const reducer = (state, action) => {
   switch (action.type) {
+    // 게임 시작
     case START_GAME:
       return {
         ...state,
         tableData: plantMine(action.row, action.cell, action.mine),
+        halted: false, // 게임 시작하면 다시 halted를 false로 변경
       };
+
+    // 일반 칸 클릭
+    case OPEN_CELL:
+      const tableData = [...state.tableData]; // tableData 얕은 복사
+      tableData[action.row] = [...state.tableData[action.row]]; // 클릭한 줄을 다시 얕은 복사 (윗줄에서는 참조 상태)
+      tableData[action.row][action.cell] = CODE.OPENED; // 클릭한 셀을 OPENED로 변경 -> 이 action을 Td에서 dispatch
+      // tableData를 변경함
+      return {
+        ...state,
+        tableData,
+      };
+
+    // 지뢰 클릭
+    case CLICK_MINE: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      tableData[action.row][action.cell] = CODE.CLICKED_MINE;
+      return {
+        ...state,
+        tableData,
+        halted: true, // 지뢰를 클릭했으므로 게임을 멈춤
+      };
+    }
+
+    // 일반칸/지뢰칸 우클릭 (깃발)
+    case FLAG_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+
+      // 깃발을 꽂을 칸이 지뢰가 있는 칸인 경우
+      if (tableData[action.row][action.cell] === CODE.MINE) {
+        tableData[action.row][action.cell] = CODE.FLAG_MINE;
+
+        // 깃발을 꽂을 칸이 일반 칸인 경우
+      } else {
+        tableData[action.row][action.cell] = CODE.FLAG;
+      }
+
+      return {
+        ...state,
+        tableData,
+      };
+    }
+
+    // 깃발칸 우클릭
+    case QUESTION_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+
+      // 지뢰가 있는 깃발 칸을 우클릭한 경우
+      if (tableData[action.row][action.cell] === CODE.FLAG_MINE) {
+        tableData[action.row][action.cell] = CODE.QUESTION_MINE;
+
+        // 지뢰가 없는 깃발 칸을 우클릭한 경우
+      } else {
+        tableData[action.row][action.cell] = CODE.QUESTION;
+      }
+
+      return {
+        ...state,
+        tableData,
+      };
+    }
+
+    // 물음표칸 우클릭
+    case NORMALIZE_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+
+      // 지뢰 칸에 물음표를 친 경우
+      if (tableData[action.row][action.cell] === CODE.QUESTION_MINE) {
+        tableData[action.row][action.cell] = CODE.MINE;
+
+        // 일반 칸에 물음표를 친 경우
+      } else {
+        tableData[action.row][action.cell] = CODE.NORMAL;
+      }
+
+      return {
+        ...state,
+        tableData,
+      };
+    }
 
     default:
       return state;
@@ -84,17 +176,20 @@ const plantMine = (row, cell, mine) => {
 const MineSearch = () => {
   // state 대신 reducer를 사용
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { tableData, halted, timer, result } = state;
 
-  // ContextAPI 성능을 위해
-  const value = useMemo(() => ({ tableData: state.tableData, dispatch }), [state.tableData]);
+  // ContextAPI 성능을 위해 props값을 memo에 넣어줌 (tableData, dispatch를 하위 컴포넌트에 전달)
+  // tableData의 값이 바뀌면 자동으로 props의 값을 update해줌
+  const value = useMemo(() => ({ tableData: tableData, halted: halted, dispatch }), [tableData, halted]);
 
   return (
     <>
+      {/* value의 값이 바뀌면 자동으로 하위 컴포넌트 리렌더링 */}
       <TableContext.Provider value={value}>
         <Form />
-        <div>{state.timer}</div>
+        <div>{timer}</div>
         <Table />
-        <div>{state.result}</div>
+        <div>{result}</div>
       </TableContext.Provider>
     </>
   );
--- 8.지뢰찾기/Td.jsx
+++ 8.지뢰찾기/Td.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { CODE, TableContext } from './MineSearch';
+import React, { useContext, useCallback } from 'react';
+import { CODE, OPEN_CELL, CLICK_MINE, FLAG_CELL, QUESTION_CELL, NORMALIZE_CELL, TableContext } from './MineSearch';
 
 const getTdStyle = (code) => {
   switch (code) {
@@ -11,9 +11,22 @@ const getTdStyle = (code) => {
       };
 
     // 칸을 열었을 때 흰 색
+    case CODE.CLICKED_MINE:
     case CODE.OPENED:
       return {
         background: 'white',
+      };
+
+    case CODE.QUESTION_MINE:
+    case CODE.QUESTION:
+      return {
+        background: 'yellow',
+      };
+
+    case CODE.FLAG_MINE:
+    case CODE.FLAG:
+      return {
+        background: 'red',
       };
 
     default:
@@ -33,6 +46,17 @@ const getTdText = (code) => {
     case CODE.MINE:
       return 'X';
 
+    case CODE.CLICKED_MINE:
+      return '펑';
+
+    case CODE.FLAG_MINE:
+    case CODE.FLAG:
+      return '!';
+
+    case CODE.QUESTION_MINE:
+    case CODE.QUESTION:
+      return '?';
+
     default:
       return '';
   }
@@ -40,8 +64,89 @@ const getTdText = (code) => {
 
 // props를 통해 해당 칸의 좌표를 알 수 있음
 const Td = ({ rowIndex, cellIndex }) => {
-  const { tableData } = useContext(TableContext);
-  return <td style={getTdStyle(tableData[rowIndex][cellIndex])}>{getTdText(tableData[rowIndex][cellIndex])}</td>;
+  const { tableData, dispatch, halted } = useContext(TableContext);
+
+  /* 
+    ! [] 안에 halted를 넣어두지 않아서 update 안되는 문제가 있었음
+  */
+
+  // 클릭 하는 경우
+  const onClickTd = useCallback(() => {
+    // 게임이 멈췄으면 아무 일도 일어나지 않도록
+    if (halted) {
+      console.log(halted);
+      return;
+    }
+
+    // 칸의 상태별로 클릭 행동을 구분
+    switch (tableData[rowIndex][cellIndex]) {
+      // 이미 열려있는 칸은 아무 효과가 없도록
+      case CODE.OPENED:
+      case CODE.FLAG_MINE:
+      case CODE.FLAG:
+      case CODE.QUESTION_MINE:
+      case CODE.QUESTION:
+        return;
+
+      // 일반 칸은 열리도록
+      case CODE.NORMAL:
+        dispatch({ type: OPEN_CELL, row: rowIndex, cell: cellIndex });
+        return;
+
+      // 지뢰칸을 클릭하는 경우 (펑 터져야 함)
+      case CODE.MINE:
+        dispatch({ type: CLICK_MINE, row: rowIndex, cell: cellIndex });
+        return;
+    }
+
+    dispatch({ type: OPEN_CELL, row: rowIndex, cell: cellIndex });
+  }, [tableData[rowIndex][cellIndex], halted]); // tableData는 클릭 시 바뀌므로 배열 안에 넣어줌
+
+  // 우클릭 하는 경우
+  const onRightClickTd = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      // 게임이 멈췄으면 아무 일도 일어나지 않도록
+      if (halted) {
+        return;
+      }
+
+      /* 
+        아래와 같이 Action을 추상적으로 그냥 만들고 나서, reducer에서 동작을 구체화할 것!
+      */
+      switch (tableData[rowIndex][cellIndex]) {
+        // 일반 셀에 우클릭한 경우 ('' -> !)
+        case CODE.NORMAL:
+        case CODE.MINE:
+          dispatch({ type: FLAG_CELL, row: rowIndex, cell: cellIndex });
+          return;
+
+        // 깃발에 우클릭한 경우 (! -> ?)
+        case CODE.FLAG_MINE:
+        case CODE.FLAG:
+          dispatch({ type: QUESTION_CELL, row: rowIndex, cell: cellIndex });
+          return;
+
+        // 물음표에 우클릭한 경우 (? -> '')
+        case CODE.QUESTION_MINE:
+        case CODE.QUESTION:
+          dispatch({ type: NORMALIZE_CELL, row: rowIndex, cell: cellIndex });
+          return;
+
+        default:
+          return;
+      }
+    },
+    [tableData[rowIndex][cellIndex], halted] // tableData는 클릭 시 바뀌므로 배열 안에 넣어줌
+  );
+
+  return (
+    // 우클릭 : onContextMenu
+    <td style={getTdStyle(tableData[rowIndex][cellIndex])} onClick={onClickTd} onContextMenu={onRightClickTd}>
+      {getTdText(tableData[rowIndex][cellIndex])}
+    </td>
+  );
 };
 
 export default Td;
